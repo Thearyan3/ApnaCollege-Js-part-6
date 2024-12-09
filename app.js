8b3d711d03fc639f50de3f440b77988ce3280982
Aryan <chaudharyaryan3456@gmail.com>
2024-12-09 20:57:31 +0530

    updated

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 function hello(){
     console.log("hello");
 }
+
+function printName(){
+    console.log("aryan");
+    console.log("aryanDon");
+}
+
+function print1to5(){
+    for(let i = 1; i<=5; i++){
+        console.log(i);
+    }
+}
+
+function isAdult(){
+    let age = 1;
+    if(age >= 18){
+        console.log("adult");
+    }else{
+        console.log("not adult");
+    }
+}
+isAdult();
