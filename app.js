8e16249ff04958cad0d92043f6fc71a8d86af5d7
Aryan <chaudharyaryan3456@gmail.com>
2024-12-09 20:59:14 +0530

    updated

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-function hello(){
-    console.log("hello");
-}
+// function hello(){
+//     console.log("hello");
+// }
 
-function printName(){
-    console.log("aryan");
-    console.log("aryanDon");
-}
+// function printName(){
+//     console.log("aryan");
+//     console.log("aryanDon");
+// }
 
-function print1to5(){
-    for(let i = 1; i<=5; i++){
-        console.log(i);
-    }
-}
+// function print1to5(){
+//     for(let i = 1; i<=5; i++){
+//         console.log(i);
+//     }
+// }
 
-function isAdult(){
-    let age = 1;
-    if(age >= 18){
-        console.log("adult");
-    }else{
-        console.log("not adult");
-    }
+// function isAdult(){
+//     let age = 1;
+//     if(age >= 18){
+//         console.log("adult");
+//     }else{
+//         console.log("not adult");
+//     }
+// }
+// isAdult();
+
+// Practice Qs - Create a function that prints a poem.
+function printPoem(){
+    console.log("Twinkle Twinkle little star");
+    console.log("How i wonder what you are ");
 }
-isAdult();
+printPoem();
