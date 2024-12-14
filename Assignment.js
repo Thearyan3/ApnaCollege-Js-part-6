2262f9f6cbc55eea9e4d1df502ad5c33a76fe4cf
Aryan <chaudharyaryan3456@gmail.com>
2024-12-14 18:44:53 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -23,33 +23,35 @@
 
 // Qs3 . Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
 // Example : country = ["Australia","Germany","United States of America"] output : "United States of America"
-// let country = ["Australia", "Germany", "United States of America"];
-// function couNtry(country) {
-//     for(let i=0; i<country.length; i++) {
-//         // console.log(country[i]);
-//         for(let j=0; j<country.length; j++) {
-//             console.log(country[i][j]);
-//         }
-//     }
-// }
-// couNtry(country);
+let country = ["Australia", "Germany", "United States of America"];
+function couNtry(country) {
+    let ansidx = 0;
+    for(let i=0; i<country.length; i++) {
+        let anslen = country[ansidx].length;
+        let currlen = country[i].length;
+        if(currlen > anslen) {
+            ansidx = i;
+        }
+    }
+    return country[ansidx];
+}
+couNtry(country);
 
 
 // Qs4 . Write a JavaScript function to count the number of vowels in a String argument.
-// let str = "MynameisAryanTanwar";
-// let vowels = "aeiouAEIOU";
-// function countVowels(str, vowels) {
-// let count = 0;
-// for(let i=0; i<str.length; i++){
-//     for(let j=0; j<vowels.length; j++){
-//         if(str[i] === vowels[j]){
-//             count++;
-//         }
-//     }
-// }
-// console.log(count);
-// }
-// countVowels(str, vowels);
+let str = "MynameisAryanTanwar";
+let vowels = "aeiouAEIOU";
+function countVowels(str, vowels) {
+let count = 0;
+for(let i=0; i<str.length; i++){
+    for(let j=0; j<vowels.length; j++){
+        if(str[i] === vowels[j]){
+            count++;
+        }
+    }
+}
+return count;
+}
 
 
 
@@ -58,7 +60,6 @@ let a = 100;
 let b = 200;
 function randomNumber(a, b){
     let diff = b-a;
-    console.log(Math.floor(Math.random()*diff)+a);
+    return Math.floor(Math.random()*diff)+a;
 }
-randomNumber(500, 1000);
 
