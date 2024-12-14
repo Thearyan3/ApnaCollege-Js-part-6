860d92d685f1018545fe1ae5bbf28061b4bf7797
Aryan <chaudharyaryan3456@gmail.com>
2024-12-14 18:19:10 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -10,13 +10,26 @@
 
 // Qs2 . Write a JavaScript function to extract unique characters from a string. 
 // Example : str = “abcdabcdefgggh” ans= “abcdefgh”
-let str = "abcdabcdefgggh";
-let ans = "";
-for(let i=0; i<str.length; i++) {
-    for(let j=0; j<str.length; j++){
-        if(str[i] === str[j]) {
-        ans += str[j];
-        }
-    }
-}
-console.log(ans);
+// let str = "abcdabcdefgggh";
+// let ans = "";
+// for(let i=0; i<str.length; i++) {
+//     for(let j=0; j<str.length; j++){
+//         if(str[i] === str[j]) {
+//         ans += str[j];
+//         }
+//     }
+// }
+// console.log(ans);
+
+// Qs3 . Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+// Example : country = ["Australia","Germany","United States of America"] output : "United States of America"
+// let country = ["Australia", "Germany", "United States of America"];
+// function couNtry(country) {
+//     for(let i=0; i<country.length; i++) {
+//         // console.log(country[i]);
+//         for(let j=0; j<country.length; j++) {
+//             console.log(country[i][j]);
+//         }
+//     }
+// }
+// couNtry(country);
