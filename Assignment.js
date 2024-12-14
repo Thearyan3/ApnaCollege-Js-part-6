fd26160aae34e210831c1c50124a022f5c1c0634
Aryan <chaudharyaryan3456@gmail.com>
2024-12-14 18:01:52 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -1,6 +1,13 @@
 // Qs1 . Write a JavaScript function that returns array elements larger than a number.
-function aRRay(n) {
-    for(let i=0; i<=n; i++){
-        return i;
-    }
-}
+// let arr = [4, 32, 55, 43, 12, 10, 75, 50, 20];
+// function aRRay(arr, n) {
+//     for(let i=0; i<=n; i++){
+//         return arr[i];
+//     }
+// }
+
+
+
+// Qs2 . Write a JavaScript function to extract unique characters from a string. 
+// Example : str = “abcdabcdefgggh” ans= “abcdefgh”
+
