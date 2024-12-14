f7f349bf870fa7a64ad43e9d3a90e33f21e4e09e
Aryan <chaudharyaryan3456@gmail.com>
2024-12-14 18:53:27 +0530

    updated

--- Assignment.js
+++ Assignment.js
@@ -1,10 +1,14 @@
 // Qs1 . Write a JavaScript function that returns array elements larger than a number.
-// let arr = [4, 32, 55, 43, 12, 10, 75, 50, 20];
-// function aRRay(arr, n) {
-//     for(let i=0; i<=n; i++){
-//         return arr[i];
-//     }
-// }
+let arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
+ let num = 5;
+  function getElements(arr, num){
+    for(let i=0; i<arr.length; i++) { 
+        if(arr[i]>num){
+            console.log(arr[i]);
+        }
+    }
+  }
+getElements(arr,num);
 
 
 
